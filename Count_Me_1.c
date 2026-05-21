45e30659586fc4ad6b457b0659da2802d0f0884d
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-05-22 00:08:15 +0600

    Add files via upload

---
+++ Count_Me_1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i = 0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+int count = 0,c3=0;
+for(int i = 0;i<n;i++){
+    if(a[i]%2==0){
+        count++;
+    }
+    else if(a[i]%3==0){
+        c3++;
+    }
+    else if(a[i]%2==0 && a[i]%3==0){
+        count++;
+    }
+}
+
+printf("%d %d\n",count,c3);
+return 0;
+}
