0504efa4aa7f08cfc067294d250fef68aefc4588
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-05-22 12:26:33 +0600

    Update Count_Me_2.c

--- Count_Me_2.c
+++ Count_Me_2.c
@@ -1,6 +1,23 @@
 #include<stdio.h>
+#include<string.h>
 int main(){
 
-    
+    char a[100001];
+    scanf("%s",a);
+
+    int count=0;
+
+    for(int i =0;i<strlen(a);i++){
+       
+    if(a[i] =='a'  || a[i] =='e'  || a[i] =='i'  || a[i] =='o' || a[i]  =='u'){
+        continue;
+    }
+    else{
+        count++;
+    }
+
+
+    }
+    printf("%d\n",count);
 return 0;
 }
